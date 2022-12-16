0edfbe8883fac8c4dbfa92e3409d3328b79d3883
Ivan Safonov <safonovivv@gmail.com>
2022-12-16 13:05:19 +0400

    added first box test

--- src/test/java/testBoxTests.java
+++ src/test/java/testBoxTests.java
@@ -1,2 +1,39 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class testBoxTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browser = "edge";
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1440x900";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+    @Test
+    void FillFormTest() {
+        String userName = "Ivan Ivanov";
+        String userEmail = "IvanIvanov@email.com";
+        String currentAddress = "city of Moscow 1";
+        String permanentAddress = "city of Moscow 2";
+
+        open("/text-box");
+        $("#userName").setValue(userName);
+        $("#userEmail").setValue(userEmail);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
+        $("#submit").click();
+
+        $("#output").shouldBe(Condition.visible);
+        $("#output #name").shouldHave(text(userName));
+        $("#output #email").shouldHave(text(userEmail));
+        $("#output #currentAddress").shouldHave(text(currentAddress));
+        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+    }
 }
+
